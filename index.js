79f89e9f51ca64c41c223c3eb7cd5cb7c7554eec
nabilasaf <nabilasafitri861@gmail.com>
2025-11-25 11:04:00 +0700

    menambahkan api key di index

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const apiRouter = require('./routers/api');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api/v1', apiRouter);
 async function startServer() {
     await connectDB();
     app.listen(PORT, () => {
